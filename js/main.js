685abe085e47aa947be0ca17457c5a7e9c1d3772
alsiendo <alsiendodewantara@gmail.com>
2020-04-24 16:04:27 +0700

    Add A Ketjeh Product Landing Page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleFunction() {
+  var burgItems = document.getElementById("hamburgerItems");
+  if (burgItems.style.display === "none") {
+    burgItems.style.display = "block";
+  } else {
+    burgItems.style.display = "none";
+  }
+}
